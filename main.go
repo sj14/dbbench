b8d59372026b47dde4829b9274e234f2eeeafe5c
Simon Jürgensmeyer <Simon.Juergensmeyer@gmail.com>
2018-11-18 22:34:03 +0100

    Goroutine flag

--- main.go
+++ main.go
@@ -20,19 +20,20 @@ type Bencher interface {
 
 func main() {
 	iterations := flag.Int("i", 1000, "how many iterations should be run")
-	dbType := flag.String("type", "", "database/driver type to use (postgres|cockroach)")
+	db := flag.String("db", "", "database/driver to use (postgres|cockroach)")
+	goroutines := flag.Int("threads", 10, "how many green threads (goroutines) to use")
 	host := flag.String("host", "localhsot", "address of the server")
 	port := flag.Int("port", 0, "port of the server")
 	user := flag.String("user", "root", "user name to connect with the server")
 	password := flag.String("password", "root", "password to connect with the server")
 	flag.Parse()
 
-	bencher := getImpl(*dbType, *host, *port, *user, *password)
+	bencher := getImpl(*db, *host, *port, *user, *password)
 
 	bencher.Setup()
 	defer bencher.Cleanup()
 
-	benchmark2(bencher, *iterations)
+	benchmark(bencher, *iterations, *goroutines)
 }
 
 func getImpl(dbType string, host string, port int, user, password string) Bencher {
@@ -47,17 +48,16 @@ func getImpl(dbType string, host string, port int, user, password string) Benche
 	return nil
 }
 
-func benchmark2(impl Bencher, iterations int) {
+func benchmark(impl Bencher, iterations, goroutines int) {
 	wg := &sync.WaitGroup{}
 
 	for _, b := range impl.Benchmarks() {
-		max := 10
-		start := time.Now()
-		wg.Add(max)
+		wg.Add(goroutines)
 		var name string
-		for i := 0; i < max; i++ {
-			from := (iterations / max) * i
-			to := (iterations / max) * (i + 1)
+		start := time.Now()
+		for i := 0; i < goroutines; i++ {
+			from := (iterations / goroutines) * i
+			to := (iterations / goroutines) * (i + 1)
 
 			go func() {
 				name = b(from, to)
