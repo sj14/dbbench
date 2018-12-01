52dff89ba4cbc3b9196ede72b703365bed48a61a
Simon Jürgensmeyer <Simon.Juergensmeyer@gmail.com>
2018-12-01 10:54:53 +0100

    First brief relational benchmark

--- databases/sqlite.go
+++ databases/sqlite.go
@@ -31,23 +31,40 @@ func NewSQLite(path string) *SQLite {
 // Benchmarks returns the individual benchmark statements for sqlite.
 func (m *SQLite) Benchmarks() []Benchmark {
 	return []Benchmark{
-		{"inserts", Loop, "INSERT INTO accounts (id, balance) VALUES( {{.Iter}}, {{call .RandInt63}});"},
-		{"selects", Loop, "SELECT * FROM accounts WHERE id = {{.Iter}};"},
-		{"updates", Loop, "UPDATE accounts SET balance = {{call .RandInt63}} WHERE id = {{.Iter}};"},
-		{"deletes", Loop, "DELETE FROM accounts WHERE id = {{.Iter}};"},
+		{"inserts", Loop, "INSERT INTO dbbench_simple (id, balance) VALUES( {{.Iter}}, {{call .RandInt63}});"},
+		{"selects", Loop, "SELECT * FROM dbbench_simple WHERE id = {{.Iter}};"},
+		{"updates", Loop, "UPDATE dbbench_simple SET balance = {{call .RandInt63}} WHERE id = {{.Iter}};"},
+		{"deletes", Loop, "DELETE FROM dbbench_simple WHERE id = {{.Iter}};"},
+		{"relation_insert0", Loop, "INSERT INTO dbbench_relational_one (oid, balance_one) VALUES( {{.Iter}}, {{call .RandInt63}});"},
+		{"relation_insert1", Loop, "INSERT INTO dbbench_relational_two (relation, balance_two) VALUES( {{.Iter}}, {{call .RandInt63}});"},
 	}
 }
 
 // Setup initializes the database for the benchmark.
 func (m *SQLite) Setup() {
-	if _, err := m.db.Exec("CREATE TABLE IF NOT EXISTS accounts (id INT PRIMARY KEY, balance DECIMAL);"); err != nil {
-		log.Fatalf("failed to create table: %v\n", err)
+	if _, err := m.db.Exec("CREATE TABLE IF NOT EXISTS dbbench_simple (id INT PRIMARY KEY, balance DECIMAL);"); err != nil {
+		log.Fatalf("failed to create table dbbench_simple: %v\n", err)
+	}
+	if _, err := m.db.Exec("CREATE TABLE IF NOT EXISTS dbbench_relational_one (oid INT PRIMARY KEY, balance_one DECIMAL);"); err != nil {
+		log.Fatalf("failed to create table dbbench_relational_one: %v\n", err)
+	}
+	if _, err := m.db.Exec("CREATE TABLE IF NOT EXISTS dbbench_relational_two (balance_two DECIMAL, relation INT, FOREIGN KEY(relation) REFERENCES dbbench_relational_one(oid));"); err != nil {
+		log.Fatalf("failed to create table dbbench_relational_two: %v\n", err)
+	}
+	if _, err := m.db.Exec("PRAGMA foreign_keys = ON;"); err != nil {
+		log.Fatalf("failed to enabled foreign keys: %v\n", err)
 	}
 }
 
 // Cleanup removes all remaining benchmarking data.
 func (m *SQLite) Cleanup() {
-	if _, err := m.db.Exec("DROP TABLE accounts"); err != nil {
+	if _, err := m.db.Exec("DROP TABLE dbbench_simple"); err != nil {
+		log.Printf("failed to drop table: %v\n", err)
+	}
+	if _, err := m.db.Exec("DROP TABLE dbbench_relational_two"); err != nil {
+		log.Printf("failed to drop table: %v\n", err)
+	}
+	if _, err := m.db.Exec("DROP TABLE dbbench_relational_one"); err != nil {
 		log.Printf("failed to drop table: %v\n", err)
 	}
 	if err := m.db.Close(); err != nil {
@@ -62,6 +79,7 @@ func (m *SQLite) Cleanup() {
 
 // Exec executes the given statement on the database.
 func (m *SQLite) Exec(stmt string) {
+	//  driver has no support for results
 	_, err := m.db.Exec(stmt)
 	if err != nil {
 		log.Printf("%v failed: %v", stmt, err)
