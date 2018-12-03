f89dc24652b3be8be5b77f23fa630239e4128903
Simon Jürgensmeyer <Simon.Juergensmeyer@gmail.com>
2018-12-03 18:01:46 +0100

    Added a block comment

--- scripts/sqlite_bench.sql
+++ scripts/sqlite_bench.sql
@@ -5,6 +5,9 @@ BEGIN TRANSACTION;
 INSERT INTO dbbench_simple (id, balance) VALUES({{.Iter}}, {{call .RandInt63}});
 DELETE FROM dbbench_simple WHERE id = {{.Iter}}; 
 COMMIT;
+/*
+INSERT INTO dbbench_simple (id, balance) VALUES("IT'S A TRAP", 1);
+*/
 \mode loop
 INSERT INTO dbbench_simple (id, balance) VALUES(1000, 1); -- inline comment
 DELETE FROM dbbench_simple WHERE id = 1000; 
