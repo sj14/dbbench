8504550e532049b9fc74353325b5f27be962bdbe
Simon Jürgensmeyer <Simon.Juergensmeyer@gmail.com>
2018-12-05 18:54:41 +0100

    Tests for loop and once

--- benchmark/benchmark_test.go
+++ benchmark/benchmark_test.go
@@ -3,7 +3,18 @@ package benchmark
 import (
 	"testing"
 	"text/template"
+
+	"github.com/stretchr/testify/mock"
 )
+
+type mockedBencher struct {
+	mock.Mock
+}
+
+func (b *mockedBencher) Benchmarks() []Benchmark { return []Benchmark{} }
+func (b *mockedBencher) Setup()                  {}
+func (b *mockedBencher) Cleanup()                {}
+func (b *mockedBencher) Exec(s string)           { _ = b.Called(s) }
 
 func TestBuildStmt(t *testing.T) {
 	// arrange
@@ -18,4 +29,34 @@ func TestBuildStmt(t *testing.T) {
 	if stmt != want {
 		t.Errorf("got statement %v, want %v", stmt, want)
 	}
+}
+
+func TestLoop(t *testing.T) {
+	// arrange
+	bencher := &mockedBencher{}
+	bencher.On("Exec", mock.Anything)
+
+	tmpl := template.New("test")
+	tmpl.Parse("{{.Iter}} {{call .RandInt63}}")
+
+	// act
+	loop(bencher, tmpl, 10, 5)
+
+	// assert
+	bencher.AssertNumberOfCalls(t, "Exec", 10)
+}
+
+func TestOnce(t *testing.T) {
+	// arrange
+	bencher := &mockedBencher{}
+	bencher.On("Exec", mock.Anything)
+
+	tmpl := template.New("test")
+	tmpl.Parse("{{.Iter}} {{call .RandInt63}}")
+
+	// act
+	once(bencher, tmpl)
+
+	// assert
+	bencher.AssertNumberOfCalls(t, "Exec", 1)
 }
